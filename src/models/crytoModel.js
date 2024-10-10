2c1bd8b9217728c0be67f31c1b6627c44273bacf
STAREBUG <shivharekartik13@gmail.com>
2024-10-10 15:54:08 +0530

    Create Crypto model
    - Defined schema for cryptocurrency data including fields for coin, price, market cap, and createdAt.
    - Added index on createdAt for efficient sorting.

---
+++ src/models/crytoModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const cryptoSchema = new mongoose.Schema({
+  coin: String,
+  price: Number,
+  marketCap: Number,
+ '24hChange' : Number,
+  timestamp: { type: Date, default: Date.now },
+});
+cryptoSchema.index({ timestamp: -1 });
+const Crypto = mongoose.model('Crypto', cryptoSchema);
+module.exports = {Crypto}; 
