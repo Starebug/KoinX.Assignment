635d42fbe0a8d756ae7a930e07c6aaa0c4fe255a
STAREBUG <shivharekartik13@gmail.com>
2024-10-10 19:14:09 +0530

    done

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async() => {
     try{
         const connection = await mongoose.connect(process.env.Mongo_URI,{});
         console.log(`MongoDB Connected : ${connection.connection.host}`)
+        console.log(process.env.MONGO_URI)
         
     }catch(e) {
         console.log("Error connecting to the database",e);
