d0b9c6c91b871c5062957b896284c110744606dd
STAREBUG <shivharekartik13@gmail.com>
2024-10-10 15:48:44 +0530

    Set up database connection - Implemented MongoDB connection using Mongoose. - Handled connection errors for better debugging.

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const connectDB = async() => {
+    try{
+        const connection = await mongoose.connect(process.env.Mongo_URI,{});
+        console.log(`MongoDB Connected : ${connection.connection.host}`)
+    }catch(e) {
+        console.log("Error connecting to the database",e);
+        process.exit();
+    }
+}
+module.exports = {connectDB};
