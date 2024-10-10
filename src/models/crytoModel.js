18b22cf0da617697da0b0ebef7d0b0078d893bb8
STAREBUG <shivharekartik13@gmail.com>
2024-10-10 20:42:50 +0530

    Create crypto Model

--- src/models/crytoModel.js
+++ src/models/crytoModel.js
@@ -4,7 +4,7 @@ const cryptoSchema = new mongoose.Schema({
   coin: String,
   price: Number,
   marketCap: Number,
- '24hChange' : Number,
+  Change24h : Number,
   timestamp: { type: Date, default: Date.now },
 });
 cryptoSchema.index({ timestamp: -1 });
